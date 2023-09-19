a4788cbd3b91823d74add52005feeb9f42ae45cf
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2023-09-19 20:57:33 +0500

    Create task2.c

---
+++ Labs/03/task2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+            int length, breadth, height, volume;
+           length=10;
+           breadth=10;
+           height=20;
+           volume= length*breadth*height;
+printf("%d",volume);
+            return 1;
+}// end main
