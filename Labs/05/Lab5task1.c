1095d418d85298894e07483b0596fb5fa7d20b09
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2023-09-19 09:06:52 +0500

    Update Lab5task1.c

--- Labs/05/Lab5task1.c
+++ Labs/05/Lab5task1.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main (){
+	int number;
+	printf("enter a number= ");
+	scanf("%d", &number);
+if (number % 3 == 0)
+	{
+	printf("The number is a multiple of 3");
+	}
+else
+	{
+	printf("The number is not a multiple of 3");
+	}
+}//end main
