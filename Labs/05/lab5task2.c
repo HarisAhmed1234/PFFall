457be3fe0df3032105c8b51477c8abfcb167ff9e
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2023-09-19 09:38:27 +0500

    Update lab5task2.c

--- Labs/05/lab5task2.c
+++ Labs/05/lab5task2.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+int main (){
+	int hour;
+	printf("enter the hour in 24 hour format= ");
+	scanf("%d", &hour);
+	if (hour>=5 && hour<=11 )
+	{
+		printf("Good Morning!");
+	}
+	else if(hour>=12 && hour<=18 )
+	{
+		printf("Good Evening!");
+	}
+	else if (hour>18 && hour<=24 )
+	{
+		printf("Good Night!");
+	}
+	else if (hour>24)
+	{
+		printf("Invalid input!");
+	}
+	}//end main
