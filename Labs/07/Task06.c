4c2a5d464358502f5aef447cbb04bda8c140af1c
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2023-10-18 20:52:16 +0500

    Create Task06.c

---
+++ Labs/07/Task06.c
@@ -0,0 +1,34 @@
+
+/*
+ * Student: Haris Ahmed
+ * Date: 18/10/2023
+ * Description: Find the frequency of number
+ */
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], N, d;
+    int freq[100] = {0};
+    do {
+        printf("Enter elements: ");
+        scanf("%d", &N);
+    } while (N > 100 || N < 1);
+
+    for (int i = 0; i < N; i++) {
+        printf("Enter element: ");
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        freq[arr[i]]++;
+    }
+
+    for (int n = 0; n < 100; n++) {
+        if (!freq[n]) continue;
+        printf("Frequency of %d = %d\n", n, freq[n]);
+    }
+
+
+    return 0;
+} // end main()
