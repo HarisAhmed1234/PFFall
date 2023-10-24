1a8cd881c763862a4b5472622fb640a638ccc322
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2023-10-24 10:14:20 +0500

    Create task4.c

---
+++ Labs/08/task4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char password[100]; 
+    char correctpassword[] = "Secure123"; 
+
+    printf("Enter your password: ");
+
+    int i = 0;
+    char c;
+
+    while (i < sizeof(password) - 1) {
+        c = getchar();
+        if (c == '\n') {
+            break; // Stop reading if Enter key is pressed
+        }
+        password[i] = c;
+        i++;
+    }
+
+    password[i] = '\0';
+
+    if (i >= 8 && strcmp(password, correctpassword) == 0) {
+        printf("Welcome!\n");
+    } else {
+        printf("Authentication failed. Please check your password.\n");
+    }
+
+    return 0;
+}
