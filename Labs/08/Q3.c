9dd5444c1c6c33910a206b0d1d307a1370673584
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2023-10-29 19:43:08 +0500

    Create Q3.c

---
+++ Labs/08/Q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter any text: ");
+    
+    if (scanf("%99[^\n]", str) == 1) {
+        char searchChar = 'a';
+        int count = 0;
+        char *ptr = str;
+
+        while ((ptr = strchr(ptr, searchChar)) != NULL) {
+            count++;
+            ptr++; // pointer moves towards the next character
+        }
+
+        printf("The character 'a' appears %d times in the input string.\n", count);
+    } else {
+        printf("Error reading input.\n");
+    }
+
+    return 0;
+}
